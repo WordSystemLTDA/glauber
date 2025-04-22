23abdc43b00b28684832ef128fe875e4f6294cb9
joaosiqueira <joaopedrochiquitin@gmail.com>
2025-04-22 14:50:44 -0300

    arrumado erro quando tem parceiros vinculados

--- lib/src/modulos/provas/interator/modelos/competidores_modelo2.dart
+++ lib/src/modulos/provas/interator/modelos/competidores_modelo2.dart
@@ -1,10 +1,11 @@
-import 'dart:convert';
+import 'package:json_annotation/json_annotation.dart';
 
-import 'package:equatable/equatable.dart';
+part 'competidores_modelo2.g.dart';
 
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 // ignore: must_be_immutable
-class CompetidoresModelo2 extends Equatable {
+@JsonSerializable()
+class CompetidoresModelo2 {
   String id;
   String nome;
   String apelido;
@@ -25,42 +26,7 @@ class CompetidoresModelo2 extends Equatable {
     required this.idParceiroTrocado,
   });
 
-  Map<String, dynamic> toMap() {
-    return <String, dynamic>{
-      'id': id,
-      'nome': nome,
-      'apelido': apelido,
-      'nomeCidade': nomeCidade,
-      'siglaEstado': siglaEstado,
-      'ativo': ativo,
-      'jaExistente': jaExistente,
-      'idParceiroTrocado': idParceiroTrocado,
-    };
-  }
+  factory CompetidoresModelo2.fromJson(Map<String, dynamic> json) => _$CompetidoresModelo2FromJson(json);
 
-  factory CompetidoresModelo2.fromMap(Map<String, dynamic> map) {
-    return CompetidoresModelo2(
-      id: map['id'] as String,
-      nome: map['nome'] as String,
-      apelido: map['apelido'] as String,
-      nomeCidade: map['nomeCidade'] as String,
-      siglaEstado: map['siglaEstado'] as String,
-      ativo: map['ativo'] as String,
-      jaExistente: map['jaExistente'] as bool,
-      idParceiroTrocado: map['idParceiroTrocado'] as String,
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory CompetidoresModelo2.fromJson(String source) => CompetidoresModelo2.fromMap(json.decode(source) as Map<String, dynamic>);
-
-  @override
-  List<Object?> get props => [
-        id,
-        nome,
-        apelido,
-        nomeCidade,
-        siglaEstado,
-      ];
+  Map<String, dynamic> toJson() => _$CompetidoresModelo2ToJson(this);
 }
--- lib/src/modulos/provas/interator/modelos/permitir_compra_modelo.g.dart
+++ lib/src/modulos/provas/interator/modelos/permitir_compra_modelo.g.dart
@@ -6,9 +6,7 @@ part of 'permitir_compra_modelo.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-PermitirCompraModelo _$PermitirCompraModeloFromJson(
-        Map<String, dynamic> json) =>
-    PermitirCompraModelo(
+PermitirCompraModelo _$PermitirCompraModeloFromJson(Map<String, dynamic> json) => PermitirCompraModelo(
       liberado: json['liberado'] as bool,
       mensagem: json['mensagem'] as String,
       rota: json['rota'] as String?,
@@ -16,16 +14,11 @@ PermitirCompraModelo _$PermitirCompraModeloFromJson(
       quantMaximaAvulsa: json['quantMaximaAvulsa'] as String?,
       quantParceiros: json['quantParceiros'] as String?,
       permVincularParceiro: json['permVincularParceiro'] as String?,
-      competidoresJaSelecionados:
-          (json['competidoresJaSelecionados'] as List<dynamic>?)
-              ?.map((e) => CompetidoresModelo2.fromJson(e as String))
-              .toList(),
+      competidoresJaSelecionados: (json['competidoresJaSelecionados'] as List<dynamic>?)?.map((e) => CompetidoresModelo2.fromJson(e as Map<String, dynamic>)).toList(),
       idCabeceiraInvalido: json['idCabeceiraInvalido'] as String?,
     );
 
-Map<String, dynamic> _$PermitirCompraModeloToJson(
-        PermitirCompraModelo instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$PermitirCompraModeloToJson(PermitirCompraModelo instance) => <String, dynamic>{
       'liberado': instance.liberado,
       'mensagem': instance.mensagem,
       'rota': instance.rota,
